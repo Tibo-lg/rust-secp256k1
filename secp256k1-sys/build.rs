297f255797aea7e4c706b3fa39115d1d9a8a688a
Tibo-lg <thibaut@cryptogarage.co.jp>
2020-10-22 11:27:42 +0900

    Update dependency folder to use ecdsa adaptor

--- secp256k1-sys/build.rs
+++ secp256k1-sys/build.rs
@@ -36,6 +36,7 @@ fn main() {
                .define("ENABLE_MODULE_ECDH", Some("1"))
                .define("ENABLE_MODULE_SCHNORRSIG", Some("1"))
                .define("ENABLE_MODULE_EXTRAKEYS", Some("1"))
+               .define("ENABLE_MODULE_ECDSA_ADAPTOR", Some("1"))
                .define("ECMULT_GEN_PREC_BITS", Some("4"))
                // TODO these three should be changed to use libgmp, at least until secp PR 290 is merged
                .define("USE_NUM_NONE", Some("1"))
